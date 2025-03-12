f071f8f2921772949dd2bc035e7b391e1f37943f
woomin <chlwoomin@naver.com>
2025-03-12 15:57:26 +0900

    프로젝트 생성

---
+++ Week2Practice/src/main/java/practice/week2practice/Week2PracticeApplication.java
@@ -0,0 +1,12 @@
+package practice.week2practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2PracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Week2PracticeApplication.class, args);
+    }
+
+}
---
+++ Week2Practice/src/test/java/practice/week2practice/Week2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.week2practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
